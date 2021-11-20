5cc507814ac65dd5623f0af606d84940ea607397
Sam Moore <sam@betterment.com>
2021-11-20 12:37:47 -0500

    rename extensions

--- lib/charlatan.dart
+++ lib/charlatan.dart
@@ -7,7 +7,7 @@ export 'src/charlatan.dart';
 export 'src/charlatan_http_client_adapter.dart';
 
 /// Utilities to make it easier to work with [Charlatan].
-extension FakeHttpExtensions on Charlatan {
+extension CharlatanExtensions on Charlatan {
   /// Builds a [CharlatanHttpClientAdapter] for a [Charlatan] instance.
   CharlatanHttpClientAdapter toFakeHttpClientAdapter() =>
       CharlatanHttpClientAdapter(this);
