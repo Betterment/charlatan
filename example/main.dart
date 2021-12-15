badff151c314189721b0b8aa9cc3243119bd2980
Sam Moore <sam@betterment.com>
2021-12-15 17:13:50 -0500

    fix(cleanup): cleanup the readme and examples (#8)

--- example/main.dart
+++ example/main.dart
@@ -32,7 +32,7 @@ void main() {
     expect(withPathParams.data, {'id': '12', 'name': 'frodo'});
   });
 
-  test('Use a custom status code and an emtpy body', () async {
+  test('Use a custom status code and an empty body', () async {
     charlatan.whenGet(
       '/posts',
       (_) => null,
@@ -42,6 +42,48 @@ void main() {
     final emptyBody = await client.get<Object?>('/posts');
     expect(emptyBody.data, '');
     expect(emptyBody.statusCode, 204);
+  });
+
+  test('Return a complicated response based on conditions', () async {
+    charlatan.whenPost('/users', (req) {
+      final data = req.body as Map<String, Object?>? ?? {};
+      final name = data['name'] as String?;
+      if (name == null) {
+        return CharlatanHttpResponse(
+          statusCode: 422,
+          body: {
+            'errors': {
+              'name': ['cannot be blank'],
+            },
+          },
+        );
+      }
+
+      return CharlatanHttpResponse(
+        statusCode: 201,
+        body: {'id': 1, 'name': name},
+      );
+    });
+
+    final invalidResponse = await client.post<Object?>(
+      '/users',
+      data: {'name': null},
+      // don't throw on non 2xx/3xx responses so that we can do assertions
+      options: Options(validateStatus: (_) => true),
+    );
+    expect(invalidResponse.data, {
+      'errors': {
+        'name': ['cannot be blank']
+      }
+    });
+    expect(invalidResponse.statusCode, 422);
+
+    final validResponse = await client.post<Object?>(
+      '/users',
+      data: {'name': 'frodo'},
+    );
+    expect(validResponse.data, {'id': 1, 'name': 'frodo'});
+    expect(validResponse.statusCode, 201);
   });
 
   test('Handle a POST and then a GET with shared state', () async {
@@ -64,8 +106,10 @@ void main() {
     final beforeCreatePost = await client.get<Object?>('/posts');
     expect(beforeCreatePost.data, {'posts': <Object?>[]});
 
-    final createPost =
-        await client.post<Object?>('/posts', data: {'name': 'bilbo'});
+    final createPost = await client.post<Object?>(
+      '/posts',
+      data: {'name': 'bilbo'},
+    );
     expect(createPost.statusCode, 204);
 
     final afterCreatePost = await client.get<Object?>('/posts');
