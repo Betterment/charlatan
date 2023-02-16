941bd2c03939d56cfdaae946abcde0358d597419
Brandon Trautmann <btrautmann@users.noreply.github.com>
2023-02-16 12:21:48 -0500

    fix tool/generate_code_coverage.sh

--- tool/generate_code_coverage.sh
+++ tool/generate_code_coverage.sh
@@ -6,4 +6,4 @@ set -o pipefail
 dart pub global activate coverage
 
 # Run tests and generate lcov
-dart test --test-randomize-ordering-seed=random --coverage=coverage && dart pub global run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --packages=.packages --report-on=lib
+dart test --coverage=coverage && dart run coverage:format_coverage --lcov --in=coverage --out=coverage/lcov.info --report-on=lib
